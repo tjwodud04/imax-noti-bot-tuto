fdcb3b038a70ce7238b617d6ccd37f080077eb25
Jae Young <kaealldays@gmail.com>
2021-11-29 02:56:29 +0900

    Update telegram_bot.py

--- cgv-noti-imax-demo/telegram_bot.py
+++ cgv-noti-imax-demo/telegram_bot.py
@@ -1,6 +1,6 @@
 import telegram
 
-chatbot = telegram.Bot(token = '2017009540:AAGQagsDwabu6ilwKrUv1TVZp0divyyJwxk')
+chatbot = telegram.Bot(token = 'telegram-personal-token')
 
 # id를 얻기 위해 해당 봇으로 메시지를 보낸 후 이걸로 아이디 확보
 # for i in chatbot.getUpdates():
@@ -8,4 +8,4 @@ chatbot = telegram.Bot(token = '2017009540:AAGQagsDwabu6ilwKrUv1TVZp0divyyJwxk')
 
 # 챗봇에서 내 아이디로 채팅 전송
 # 여기서 아이디값은 메시지 보낸 후 전달받은 아이디 입력
-chatbot.sendMessage(chat_id= 54133053, text = "챗봇 챗봇")
+chatbot.sendMessage(chat_id= telegram-id-number, text = "챗봇 챗봇")
